b446c3a4ebb5cf78083bead90c042b253544c2c0
brunvelop <brunvelop@gmail.com>
2021-12-19 18:05:48 +0100

    feat: Add ERC721 mint, maxSupply and Ownable

--- contracts/MyNft.sol
+++ contracts/MyNft.sol
@@ -1,9 +1,43 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyNft is ERC721{
-    constructor() ERC721("MyNft", "NFT") {}
+contract MyToken is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uint256 public maxSupply;
+
+    constructor(uint256 _maxSupply) ERC721("MyToken", "MTK") {
+        maxSupply = _maxSupply;
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        require(tokenId < maxSupply, "Sold Out :(");
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
-
